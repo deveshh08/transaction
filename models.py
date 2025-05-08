533c2f758b99d993d2a4cffb63828db86c505a7c
deveshh08 <deveshrajwani933@gmail.com>
2025-05-08 16:58:35 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,11 @@
+# models.py
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class Plan(db.Model):
+    __tablename__ = 'plan'  # optional, sets custom table name
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), nullable=False)
     plan_name = db.Column(db.String(100), nullable=False)
